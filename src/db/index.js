47edd9b23bed2d3cc6d69542593b04b55e561ea9
shreyachangole <your-email@example.com>
2025-06-21 02:50:18 +0530

    db get connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
- const connectDB = async () => {
-    
- 
-try {
-   const ConnectionInstanse= await mongoose.connect(`{$process.env.MONGODB_URL}/{$DB_NAME}`)
-   console.log(`/n"database get connceted DB HOST", ${ConnectionInstanse.connection.host}`)
-} catch (error) {
-    console.log("Error in DB",error)
-    process.exit(1)
-    
-}}
-export default  connectDB
+
+const connectDB = async () => {
+  try {
+    const ConnectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+
+    console.log(`\n✅ Database connected to DB HOST: ${ConnectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("❌ Error in DB", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import connectDB from
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from './app.js'; 
+
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
